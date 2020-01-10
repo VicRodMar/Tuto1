95495a1cfae2707a203fa9081a2ada558cc4902f
VicRodMar <vic_rodriguez94@hotmail.com>
2020-01-10 17:40:43 +0100

    Essai d'ajouter la fonctionnalité des cookies: non reussi

--- Tuto2.js
+++ Tuto2.js
@@ -1,11 +1,21 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+const LastGuess = document.querySelector('.LastGuess');
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
+const NameSubmit = document.querySelector('.NameSubmit');
+const NameField = document.querySelector('.NameField');
+const HighestScoreField = document.querySelector('.HighestScoreField');
 let guessCount = 1;
+let Score = 0;
+let HighestScore = 0;
+let HighestScoreName = 'NaN';
 let resetButton;
+let userName;
+let TableCookie;
+
 
 function checkGuess() {
   let userGuess = Number(guessField.value);
@@ -13,7 +23,15 @@ function checkGuess() {
     guesses.textContent = 'Previous guesses: ';
   }
 
-  guesses.textContent += userGuess + ' ';
+  if(userGuess === 69)
+  {
+    LastGuess.textContent = 'Thibault ne fait pas le malin :)';
+  }
+  else{
+    LastGuess.textContent = 'Last Guess: ' + userGuess;
+  }
+  
+    guesses.textContent += userGuess + ' ';
 
   if (userGuess === randomNumber) {
     lastResult.textContent = 'Congratulations! You got it right!';
@@ -21,12 +39,13 @@ function checkGuess() {
     lowOrHi.textContent = '';
     setGameOver();
   } else if (guessCount === 10) {
-    lastResult.textContent = '!!!GAME OVER!!!';
+    lastResult.textContent = '¡¡¡GAME OVER!!!';
     lowOrHi.textContent = '';
     setGameOver();
   } else {
     lastResult.textContent = 'Wrong!';
     lastResult.style.backgroundColor = 'red';
+
     if(userGuess < randomNumber) {
       lowOrHi.textContent = 'Last guess was too low!' ;
     } else if(userGuess > randomNumber) {
@@ -39,9 +58,11 @@ function checkGuess() {
   guessField.focus();
 }
 
-guessSubmit.addEventListener('click', checkGuess);
+
 
 function setGameOver() {
+  showScores();
+
   guessField.disabled = true;
   guessSubmit.disabled = true;
   resetButton = document.createElement('button');
@@ -57,11 +78,81 @@ function resetGame() {
     resetParas[i].textContent = '';
   }
 
-  resetButton.parentNode.removeChild(resetButton);
+  
+  resetButton.parentNode.removeChild(resetButton);  
   guessField.disabled = false;
   guessSubmit.disabled = false;
   guessField.value = '';
   guessField.focus();
+  NameField.disabled = false;
+  NameSubmit.disabled = false;
+  NameField.value = '';
+  NameField.focus();
   lastResult.style.backgroundColor = 'white';
   randomNumber = Math.floor(Math.random() * 100) + 1;
 }
+
+function showScores()
+{
+  var table = document.getElementById("myTable");
+
+  // Create an empty <tr> element and add it to the 1st position of the table:
+  var row = table.insertRow(table.row);
+
+  // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  Score = 10 - (guessCount);
+  // Add some text to the new cells:
+  cell1.innerHTML = userName;
+  cell2.innerHTML = Score;
+
+  compareScore();
+}
+
+function StockName()
+{
+  userName = NameField.value;
+  NameField.disabled = true;
+  NameSubmit.disabled = true;
+  
+  var d = new Date();
+  d.setTime(d.getTime() + (24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  var table = document.getElementById("myTable");
+  //document.cookie = table. + ";" + expires;
+  TableCookie = document.cookie;
+
+}
+
+function compareScore()
+{
+  if(Score > HighestScore)
+  {
+    HighestScore = Score;
+    HighestScoreName = userName;    
+  }
+  HighestScoreField.textContent = 'The highest score is: ' + HighestScore + ' made by: ' + HighestScoreName;
+}
+
+function WriteTable(){
+  var table = document.getElementById("myTable");
+  var tableCookie = document.cookie;
+  for(let i = 0 ; i < tableCookie.length ; i++) {
+     // Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(table.row);
+
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    // Add some text to the new cells:
+    cell1.innerHTML = userName;
+    cell2.innerHTML = Score;
+  }
+
+}
+guessSubmit.addEventListener('click', checkGuess);
+NameSubmit.addEventListener('click', StockName);
+
+window.onload = WriteTable;
