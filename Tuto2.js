01361e6594b79efee24e4b34607f96973a94909c
VicRodMar <vic_rodriguez94@hotmail.com>
2020-01-10 14:48:11 +0100

    Ajout du script pour jouer

--- Tuto2.js
+++ Tuto2.js
@@ -1,41 +1,67 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+let guessCount = 1;
+let resetButton;
 
-const Example = (props) => {
-  return (
-    <Container>
-      <Row xs="2">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="3">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col xs="6">Column</Col>
-        <Col>Column</Col>
-      </Row>
-      <Row xs="1" sm="2" md="4">
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-        <Col>Column</Col>
-      </Row>
-    </Container>
-  );
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!' ;
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
+  }
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
 }
 
-export default Example;
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  document.body.appendChild(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for(let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+  lastResult.style.backgroundColor = 'white';
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
